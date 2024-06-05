40e66a3615e639468c0ff65f11859cf8cc2643ae
marou <marou@OPA>
2024-06-05 19:33:20 +0200

    ola

--- src/ola/iban.java
+++ src/ola/iban.java
@@ -4,6 +4,7 @@ public class iban {
 	public static void main(String[] args) {
 		System.out.println("hola soy ibai llanos dame tu dinero");
 		System.out.println("el tigre");
+		System.out.println("ibai");
 	}
 
 }
